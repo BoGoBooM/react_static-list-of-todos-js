7c2a9f31f64d770061116fde1d24ed4d5906b0dd
BoGoBooM <myPikcha@gmail.com>
2025-04-10 21:11:13 +0300

    task solution

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,2 +1,12 @@
+import { TodoInfo } from '../TodoInfo';
+
 // Add the required props
-export const TodoList = () => <>TodoList markup</>;
+export const TodoList = ({ todos }) => (
+  <>
+    {todos.map(todo => (
+      <section className="TodoList">
+        <TodoInfo todo={todo} key={todo.id} />
+      </section>
+    ))}
+  </>
+);
